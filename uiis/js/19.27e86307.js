976f5302b7ddc62e41aa191da35a07434f366485
RedwanPlague <mdredwan.rh@gmail.com>
2021-06-26 18:47:27 +0600

    deploying

--- uiis/js/19.27e86307.js
+++
@@ -1,1 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[19],{e71d:function(e,n,t){"use strict";t.r(n);var r=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("router-view")},u=[],c={name:"AccountsWrapper"},a=c,l=t("2877"),o=Object(l["a"])(a,r,u,!1,null,null,null);n["default"]=o.exports}}]);
