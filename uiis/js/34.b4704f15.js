b8f11500efa054ac73f585d6a7a1fac3377a6751
RedwanPlague <mdredwan.rh@gmail.com>
2021-06-27 21:19:56 +0600

    deploying

--- uiis/js/34.b4704f15.js
+++
@@ -1,1 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[34],{bb02:function(e,n,r){"use strict";r.r(n);var t=function(){var e=this,n=e.$createElement,r=e._self._c||n;return r("router-view")},u=[],a={name:"FeesWrapper"},l=a,s=r("2877"),c=Object(s["a"])(l,t,u,!1,null,null,null);n["default"]=c.exports}}]);
---
+++ uiis/js/39.9b97cfd8.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[39],{ab62:function(e,n,r){"use strict";r.r(n);var t=function(){var e=this,n=e.$createElement,r=e._self._c||n;return r("router-view")},a=[],u={name:"FinesWrapper"},l=u,s=r("2877"),c=Object(s["a"])(l,t,a,!1,null,null,null);n["default"]=c.exports}}]);
